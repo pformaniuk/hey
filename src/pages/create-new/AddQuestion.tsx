0d005b8cb255424ba8421a2badd2cb6557515c33
Pavel Formaniuk <pformaniuk@exadel.com>
2021-04-28 01:09:00 +0300

    draft of add questions

--- src/pages/create-new/AddQuestion.tsx
+++ src/pages/create-new/AddQuestion.tsx
@@ -1,3 +1,67 @@
+import { Container, Button } from '@material-ui/core';
+import { useCallback, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 export const AddQuestion = () => {
-    return (<div>AddQuestion</div>);
+  const history = useHistory();
+  const handleClick = useCallback(() => history.push('/'), [history]);
+  const [inputFields, setInputFields] = useState<string[]>(['']);
+
+  const handleInputChange = (index: number, event: any) => {
+    const values = [...inputFields];
+
+    values[index] = event.target.value;
+    
+    setInputFields(values);
+  };
+
+  const handleAddFields = () => {
+    const values = [...inputFields];
+    values.push('');
+    setInputFields(values);
+  };
+
+  const handleRemoveFields = (index:number) => {
+    const values = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  };
+
+
+  const inputs = inputFields.map((el, index) => {
+    return (
+      <>
+        <div>
+          <label>
+            Question:
+          <input
+              type="text"
+              className="form-control"
+              id="firstName"
+              name="firstName"
+              value={el}
+              onChange={event => handleInputChange(index, event)}
+            />
+          </label>
+          <div>
+            <Button onClick={handleAddFields}> + </Button>
+            {index > 0 && <Button onClick={() => {handleRemoveFields(index)} }> - </Button>}
+          </div>
+        </div>
+      </>
+    );
+  });
+
+  return (
+    <Container maxWidth="md">
+      <div >
+
+        <form>
+          {inputs}
+        </form>
+
+        <Button variant="contained" color="primary" onClick={handleClick}>Save Vote </Button>
+      </div>
+    </Container>
+  );
 }
